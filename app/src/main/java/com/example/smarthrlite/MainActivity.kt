e164a189d19de6d0716d6aae885c5763627bd2cd
YusufQureshi19 <mryusufqureshi.online@gmail.com>
2026-04-23 18:49:16 -0600

    Added small update

--- app/src/main/java/com/example/smarthrlite/MainActivity.kt
+++ app/src/main/java/com/example/smarthrlite/MainActivity.kt
@@ -9,7 +9,7 @@ import com.example.smarthrlite.ui.theme.SmartHRLiteTheme
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)                                   //yusufqureshi
+        super.onCreate(savedInstanceState)
 
         setContent {
             SmartHRLiteTheme {
